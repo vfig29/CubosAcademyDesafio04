6a506d20c2cfa59164d8eb3498bf9127f99c8372
vfig29 <eliotefig@hotmail.com>
2021-04-01 20:32:29 -0300

    Aula 04: Desafio (Somador)

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,18 +16,53 @@ class CounterScreen extends StatefulWidget {
 }
 
 class _CounterScreenState extends State<CounterScreen> {
+  var resultado = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Meu Contador'),
       ),
-      body: Container(
-        color: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CounterDisplay(
+            text: 'Resultado',
+            fontSize: 26,
+          ),
+          CounterDisplay(
+            text: '$resultado',
+            fontSize: 36,
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => print("Pressionado"),
+        onPressed: () {
+          setState(() {
+            resultado++;
+            print('funciona!');
+          });
+        },
         child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class CounterDisplay extends StatelessWidget {
+  final String text;
+  final double fontSize;
+
+  CounterDisplay({Key key, this.text = '', this.fontSize = 1.0})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        text,
+        style: TextStyle(
+          fontSize: fontSize,
+        ),
       ),
     );
   }
